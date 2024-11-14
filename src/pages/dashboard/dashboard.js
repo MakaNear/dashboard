1a8b8d2e6a1f68467c05a9f319785eb77882a351
mieramensatu <sasakihaise985@gmail.com>
2024-11-15 02:10:55 +0700

    fix: try debugging deploy

--- src/pages/dashboard/dashboard.js
+++ src/pages/dashboard/dashboard.js
@@ -4,10 +4,10 @@ import { url } from "/src/helpers/urlConfig.js";
 
 export async function main() {
   const promises = [
-    loadComponent("header.topbar", url.components.topbar + "topbar.html"),
-    loadComponent("aside.sidebar", url.components.sidebar + "sidebar.html"),
+    loadComponent(".dashboard .topbar", url.components.topbar + "topbar.html"),
+    loadComponent(".dashboard .sidebar", url.components.sidebar + "sidebar.html"),
     loadComponent(".dashboard .content", url.pages.dashboard + "content/content.html"),
-    loadComponent("footer.footer", url.components.footer + "footer.html"),
+    loadComponent(".dashboard .footer", url.components.footer + "footer.html"),
   ];
 
   Promise.all(promises)

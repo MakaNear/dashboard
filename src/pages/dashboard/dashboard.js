a23b0b864c1ce472db86533352aed3f5c739c9d3
mieramensatu <sasakihaise985@gmail.com>
2024-12-06 17:37:37 +0700

    fix: implement custom getCookie function for token retrieval and redirect to login if not authenticated

--- src/pages/dashboard/dashboard.js
+++ src/pages/dashboard/dashboard.js
@@ -4,7 +4,23 @@ import { url } from "../../helpers/urlConfig.js";
 import sidebarTogle from "../../components/sidebar/sidebar.js";
 import FetchProfileTopbar from "../../components/topbar/profile.js";
 
+function getCookie(name) {
+  const cookies = document.cookie.split("; ");
+  for (let cookie of cookies) {
+    const [key, value] = cookie.split("=");
+    if (key === name) {
+      return value;
+    }
+  }
+  return null;
+}
 export async function main() {
+
+  const token = getCookie("token");
+  if (!token) {
+    window.location.href = "/login";
+    return;
+  }
   const promises = [
     loadComponent("header.topbar", url.components.topbar + "topbar.html"),
     loadComponent("aside.sidebar", url.components.sidebar + "sidebar.html"),

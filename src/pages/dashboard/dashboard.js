475df30a148c69bf6c90a68317eb7089120f428a
mieramensatu <sasakihaise985@gmail.com>
2024-12-06 15:48:52 +0700

    fix: import and invoke sidebarToggle in the main function of dashboard page

--- src/pages/dashboard/dashboard.js
+++ src/pages/dashboard/dashboard.js
@@ -1,6 +1,7 @@
 import loadComponent from "../../helpers/loadComponent.js";
 import { smoothScroll } from "../../helpers/smoothScroll.js";
 import { url } from "../../helpers/urlConfig.js";
+import sidebarTogle from "../../components/sidebar/sidebar.js";
 
 export async function main() {
   const promises = [
@@ -18,6 +19,7 @@ export async function main() {
       console.log(url.components.topbar + "topbar.html");
       console.log(url.components.sidebar + "sidebar.html");
       console.log(url.pages.dashboard + "content/content.html");
+      sidebarTogle();
     })
     .catch((error) => {
       console.error("Error loading components:", error);

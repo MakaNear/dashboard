1ec58294084b512f42a927330448b69e447882f6
mieramensatu <sasakihaise985@gmail.com>
2024-12-06 15:32:12 +0700

    fix: refactor sidebarToggle function, enhance menu item activation and improve logout confirmation dialog

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -1,69 +1,67 @@
-export async function navbar() {
-  const list = document.querySelectorAll(".navbar .nav .nav-main .links a");
+export default function sidebarTogle() {
+  const list = document.querySelectorAll(
+    ".app-header .app-header-navigation .submenu a"
+  );
 
   list.forEach((link) => {
     const href = link.getAttribute("href");
 
-    if (window.location.pathname === "/" && href === "/") {
+    if (window.location.pathname === "/dashboard/" && href === "/dashboard") {
       link.classList.add("active");
     }
   });
 
-  // ----- Display Logout Button ----- //
-  const loginCookie = window.Cookies.get("login"); // Access Cookies from window object
-  const logoutButton = document.querySelector(".nav-others .logout");
-  const loginButton = document.querySelector(".nav-others .to-login");
+  const menuItems = document.querySelectorAll("aside.navigation .menu-item");
+  if (menuItems) {
+    menuItems.forEach((menu) => {
+      menu.addEventListener("click", function (e) {
+        const menuName = this.getAttribute("data-menu");
 
-  if (logoutButton) {
-    if (loginCookie) {
-      logoutButton.style.display = "inline-block";
-      loginButton.style.display = "none";
-    } else {
-      logoutButton.style.display = "none";
-      loginButton.style.display = "inline-block";
-    }
-
-    // ----- Logout ----- //
-    logoutButton.addEventListener("click", async (event) => {
-      event.preventDefault(); 
-      const result = await Swal.fire({
-        title: "Logout Confirmation",
-        text: "Are you sure you want to log out?",
-        icon: "warning",
-        showCancelButton: true,
-        confirmButtonColor: "#3085d6",
-        cancelButtonColor: "#d33",
-        confirmButtonText: "Yes, log out!",
-        cancelButtonText: "Cancel",
+        localStorage.setItem("activeMenu", menuName);
       });
-
-      if (result.isConfirmed) {
-        // Remove token or cookie from the header
-        try {
-          const token = window.Cookies.get("token"); // Assuming token is stored in cookies
-          if (token) {
-            window.Cookies.remove("token"); // Remove token
-          }
-
-          Swal.fire({
-            title: "Logged Out!",
-            text: "You have successfully logged out.",
-            icon: "success",
-            showConfirmButton: false,
-            timer: 2000, // Show alert for 2 seconds
-          }).then(() => {
-            window.Cookies.remove("login"); // Remove login cookie
-            window.location.href = "/login"; // Redirect to login page
-          });
-        } catch (error) {
-          Swal.fire({
-            title: "Error!",
-            text: "Failed to log out. Please try again.",
-            icon: "error",
-            confirmButtonColor: "#d33",
-          });
-        }
-      }
     });
   }
+
+  const activeMenu = localStorage.getItem("activeMenu");
+  if (activeMenu) {
+    document
+      .querySelectorAll(".app-header .app-header-navigation .submenu")
+      .forEach((submenu) => {
+        submenu.style.display = "none";
+      });
+
+    const activeSubmenu = document.querySelector(`.${activeMenu}-submenu`);
+    if (activeSubmenu) {
+      activeSubmenu.style.display = "flex";
+    }
+  }
+
+  const logoutButton = document.querySelector(".navigation-nav .logout");
+
+  logoutButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    Swal.fire({
+      title: "Logout Confirmation",
+      text: "Are you sure you want to log out?",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, log out!",
+      cancelButtonText: "Cancel",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire({
+          title: "Logged Out!",
+          text: "You have successfully logged out.",
+          icon: "success",
+          showConfirmButton: false,
+          timer: 2000, // Show alert for 2 seconds
+        }).then(() => {
+          window.Cookies.remove("login"); // Remove login cookie
+          window.location.href = "/login"; // Redirect to login page
+        });
+      }
+    });
+  });
 }
